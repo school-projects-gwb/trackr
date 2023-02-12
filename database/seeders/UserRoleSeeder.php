7271089c0106757a4b17be80855519fe89016553
gunwunbun <jvanhouts@protonmail.com>
2023-02-12 17:43:03 +0100

    added role and user seeders

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'SuperAdmin']);
+        Role::create(['name' => 'StoreAdmin']);
+        Role::create(['name' => 'StorePacker']);
+    }
+}
