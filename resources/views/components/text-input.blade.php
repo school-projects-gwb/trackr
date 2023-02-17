41e9ba0fe3521db53a2f22fb1c01dfbe50b37cdd
gunwunbun <jvanhouts@protonmail.com>
2023-02-17 08:36:13 +0100

    added tracking element to homepage

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'bg-white border-0 rounded-xl py-2']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'bg-white border-0 rounded-xl py-2 pl-4']) !!}>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,24 @@
 <x-site-layout>
-    <div class="h-full w-1/2 bg-gray-100"></div>
-    <div class="h-full w-1/2 bg-secondary-lighter"></div>
+    <div class="min-h-full w-full xl:w-1/2 bg-gray-100 flex items-center justify-center">
+        <div class="flex flex-col w-11/12 xl:w-11/12">
+            <div class="flex flex-col items-center xl:items-start mt-8">
+                <h1 class="text-4xl font-bold tracking-tight">Track & Trace</h1>
+                <h3 class="mt-2 text-xl">Vul je gegevens in om je pakket in te zien!</h3>
+            </div>
+            <div class="flex flex-col xl:flex-row items-center xl:items-end mt-8">
+                <div class="w-3/4 xl:w-6/12 flex flex-col mr-0 xl:mr-4">
+                    <x-input-label for="tracking_id" :value="__('TrackR ID')" />
+                    <x-text-input id="tracking_id" />
+                </div>
+                <div class="w-3/4 xl:w-4/12 flex flex-col mr-0 xl:mr-4 mt-8 xl:mt-0">
+                    <x-input-label for="zip_code" :value="__('Postcode')" />
+                    <x-text-input id="zip_code" placeholder="1234 AB" />
+                </div>
+                <x-button-primary class="w-3/4 xl:w-2/12 mt-8 xl:mt-0">
+                    {{ __('Zoeken') }}
+                </x-button-primary>
+            </div>
+        </div>
+    </div>
+    <div class="min-h-full w-full xl:w-1/2 bg-secondary-lighter"></div>
 </x-site-layout>
