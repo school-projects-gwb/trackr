e3d489201c99d07d9623707429d9d16226de5830
gunwunbun <jvanhouts@protonmail.com>
2023-03-04 12:26:18 +0100

    fixed dashboard page responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,30 +14,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-
-    @if (!request()->routeIs('dashboard'))
-        <div class="min-h-screen bg-primary">
-            @include('layouts.navigation')
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    @else
-        <div class="w-full h-full bg-primary flex">
+        <div class="w-full min-h-full bg-primary flex flex-col lg:flex-row">
             @include('layouts.navigation-sidebar')
-            <div class="w-3/4 pt-16 pl-16">
+            <div class="w-full lg:w-3/4 p-4 lg:pb-0 lg:pr-0 lg:pt-16 lg:pl-16">
                 <h1 class="text-3xl font-semibold tracking-tight">Dashboard</h1>
             </div>
         </div>
-    @endif
     </body>
 </html>
